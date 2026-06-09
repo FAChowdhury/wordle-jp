e48c7932d02625c6b600a045697524f85cef68e1
Farhan <chowdhury.farhan235@gmail.com>
2026-06-09 22:32:42 +1000

    created insert statements for dictionary schema for kanji, kunyomi and onyomi.

---
+++ db/japanese/dictionary/kanji_kunyomi/insert_kanji_kunyomi.sql
@@ -0,0 +1,79 @@
+-- id will be <kanji_id> * 100 + <two digit number (01, 02 ... 99)>
+
+-- Kanji 1: 左 (kanji_id = 1)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (101, 1, 'ひだり'); -- 左
+
+-- Kanji 2: 右 (kanji_id = 2)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (201, 2, 'みぎ'); -- 右
+
+-- Kanji 3: 友 (kanji_id = 3)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (301, 3, 'とも'); -- 友
+
+-- Kanji 4: 気 (kanji_id = 4) (NONE)
+
+-- Kanji 5: 六 (kanji_id = 5)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (501, 5, 'む'); -- 六
+
+-- Kanji 6: 休 (kanji_id = 6)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (601, 6, 'やす'); -- 休
+
+-- Kanji 7: 才 (kanji_id = 7)　(NONE)
+
+
+-- Kanji 8: 分 (kanji_id = 8)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (801, 8, 'わ'); -- 分
+
+-- Kanji 9: 何 (kanji_id = 9)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (901, 9, 'なに'); -- 何
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (902, 9, 'なん'); -- 何
+
+-- Kanji 10: 谷 (kanji_id = 10)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (1001, 10, 'たに'); -- 谷
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (1002, 10, 'や'); -- 谷
+
+-- Kanji 11: 朝 (kanji_id = 11)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (1101, 11, 'あさ'); -- 朝
+
+-- Kanji 12: 安 (kanji_id = 12)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (1201, 12, 'やす'); -- 安
+
+-- Kanji 13: 妹 (kanji_id = 13)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (1301, 13, 'いもうと'); -- 妹
+
+-- Kanji 14: 札 (kanji_id = 14)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (1401, 14, 'ふだ'); -- 札
+
+-- Kanji 15: 四 (kanji_id = 15)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (1501, 15, 'よん'); -- 四
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (1502, 15, 'よ'); -- 四
+
+-- Kanji 16: 礼 (kanji_id = 16) (NONE)
+
+
+-- Kanji 17: 花 (kanji_id = 17)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (1701, 17, 'はな'); -- 花
+
+-- Kanji 18: 菜 (kanji_id = 18)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (1801, 18, 'な'); -- 菜
+
+-- Kanji 19: 妥 (kanji_id = 19) (NONE)
+
+
+-- Kanji 20: 好 (kanji_id = 20)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (2001, 20, 'す'); -- 好
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (2002, 20, 'この'); -- 好
+
+-- Kanji 21: 町 (kanji_id = 21)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (2101, 21, 'まち'); -- 町
+
+-- Kanji 22: 猫 (kanji_id = 22)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (2201, 22, 'ねこ'); -- 猫
+
+-- Kanji 23: 俺 (kanji_id = 23)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (2301, 23, 'おれ'); -- 俺
+
+-- Kanji 24: 孔 (kanji_id = 24)
+INSERT INTO dictionary__kanji_kunyomi (id, kanji_id, reading) VALUES (2401, 24, 'あな'); -- 孔
+
+-- Kanji 25: 茶 (kanji_id = 25) (NONE)
+
