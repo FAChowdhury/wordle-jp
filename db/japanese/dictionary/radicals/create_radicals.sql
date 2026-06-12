6d5ce3e619dd0e6e5d32132bf536cb90a3653d1a
Farhan <chowdhury.farhan235@gmail.com>
2026-06-13 09:42:26 +1000

    add insert statements for dictionary__radicals as insert_radicals.sql

--- db/japanese/dictionary/radicals/create_radicals.sql
+++ db/japanese/dictionary/radicals/create_radicals.sql
@@ -4,7 +4,7 @@ CREATE TABLE dictionary__radicals (
     radical_id INTEGER NOT NULL UNIQUE,
     
     en_name TEXT NOT NULL,
-    reading TEXT NOT NULL,
+    reading TEXT,
     stroke_count INTEGER,
 
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
